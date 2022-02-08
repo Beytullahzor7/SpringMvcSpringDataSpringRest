ffaeea52343e830d520023a89fea031168714390
Beytullahzor7 <beytullahzor7@gmail.com>
2022-02-08 20:28:34 +0300

    Success page uploaded

--- src/main/java/com/innova/dto/InnovaValidationDto.java
+++ src/main/java/com/innova/dto/InnovaValidationDto.java
@@ -7,6 +7,8 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 
+import javax.validation.constraints.*;
+
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
@@ -14,9 +16,21 @@ import lombok.extern.log4j.Log4j2;
 @Log4j2
 public class InnovaValidationDto {
 
+    @NotEmpty(message = "Username alanı bos gecilemez")
     private String userName;
+
+    @NotEmpty(message = "Usersurname alanı bos gecilemez")
     private String userSurname;
+
+    @NotEmpty(message = "Email alanı bos gecilemez")
+    @Email(message = "uygun formatta mail giriniz")
     private String emailAddress;
+
+    @Min(value = 18, message = "18 yasından kücüksünüz bu yüzden basvuramazsınız")
+    @Max(value = 45, message = "45 yasından büyüksünüz bu yüzden basvuramazsınız")
     private int userAge;
+
+    @NotEmpty(message = "Message alanı bos gecilemez")
+    @Size(min=1, max=255)
     private String userMessage;
 }
