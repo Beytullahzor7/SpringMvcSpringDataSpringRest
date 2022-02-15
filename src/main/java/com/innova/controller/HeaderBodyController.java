3e1ab20ce9d5e7038fbc5e072ab36e632b6a4f22
Beytullahzor7 <beytullahzor7@gmail.com>
2022-02-15 19:12:23 +0300

    ResponseCookie

--- src/main/java/com/innova/controller/HeaderBodyController.java
+++ src/main/java/com/innova/controller/HeaderBodyController.java
@@ -47,4 +47,37 @@ public class HeaderBodyController {
         String body = data.getBody();
         return "@Controller(Client): " + body + " " + gelenData;
     }
+
+    //REQUEST COOKIE
+    // http://localhost:8080/controller/client/cookie
+    @GetMapping("/controller/client/cookie")
+    @ResponseBody
+    public String getRequestCookie(){
+        String URL = "http://localhost:8080/service/client/cookie";
+        RestTemplate restTemplate = new RestTemplate();
+
+        HttpHeaders httpHeaders = new HttpHeaders();
+        httpHeaders.add(HttpHeaders.COOKIE, "key_cookie=cookieValue");
+
+        HttpEntity<String> httpEntity = new HttpEntity<>("Bu data verisi", httpHeaders);
+
+        ResponseEntity<String> responseEntity = restTemplate.exchange(URL, HttpMethod.GET, httpEntity, String.class);
+        String result = responseEntity.getBody();
+        return "@Controller(Client): " + result;
+    }
+
+    //REQUEST COOKIE
+    // http://localhost:8080/controller/response/cookie
+    @GetMapping("/controller/response/cookie")
+    @ResponseBody
+    public String getResponseCookie(){
+        String URL = "http://localhost:8080/service/response/cookie";
+        RestTemplate restTemplate = new RestTemplate();
+
+        ResponseEntity<String> data = restTemplate.exchange(URL, HttpMethod.GET, HttpEntity.EMPTY, String.class);
+        String gelenData = data.getHeaders().getFirst(HttpHeaders.SET_COOKIE);
+        String body = data.getBody();
+
+        return "@Controller(Client): " + body + " " + gelenData;
+    }
 }
--- src/main/java/com/innova/services/HeaderBodyServices.java
+++ src/main/java/com/innova/services/HeaderBodyServices.java
@@ -1,7 +1,9 @@
 package com.innova.services;
 
 import lombok.extern.log4j.Log4j2;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
@@ -28,6 +30,25 @@ public class HeaderBodyServices {
                 .ok()
                 .header("key_response", "Serverdan gelen response")
                 .body("@RestController: gelen veri");
+    }
 
+    // REQUEST COOKIE
+    // Client cookie olusturup servera gönderir
+    // http://localhost:8080/service/client/cookie
+    @GetMapping("/service/client/cookie")
+    public ResponseEntity<?> getRequestCookie(@CookieValue(value = "key_cookie", defaultValue = "default cookie") String gelenCookie){
+        log.info("@RestController(Server): "+ gelenCookie);
+        return ResponseEntity.ok(gelenCookie);
+    }
+
+    // RESPONSE COOKIE
+    // Server cookie olusturup clienta gönderir
+    // http://localhost:8080/service/response/cookie
+    @GetMapping("/service/response/cookie")
+    public ResponseEntity<?> getResponseCookie(){
+        return ResponseEntity
+                .ok()
+                .header(HttpHeaders.SET_COOKIE, "key_response_cookie")
+                .body("@RestController: gelen cookie verisi");
     }
 }
