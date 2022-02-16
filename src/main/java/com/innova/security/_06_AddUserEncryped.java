0f3b28abb625483fc66207337d8388494fc49b5f
Beytullahzor7 <beytullahzor7@gmail.com>
2022-02-16 17:01:31 +0300

    NoEncryped, Encryped, Bean

---
+++ src/main/java/com/innova/security/_06_AddUserEncryped.java
@@ -0,0 +1,24 @@
+package com.innova.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+//@EnableWebSecurity
+public class _06_AddUserEncryped extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder authenticationManagerBuilder) throws Exception {
+
+        PasswordEncoder passwordEncoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
+
+        authenticationManagerBuilder
+                .inMemoryAuthentication() //bellek üzerinde olsun db olmasın
+                .withUser("root") //username
+                .password(passwordEncoder.encode("root")) //sifrelenmis data
+                .roles("USER");
+    }
+}
---
+++ src/main/java/com/innova/security/_07_AddUserEncrypedBean.java
@@ -0,0 +1,28 @@
+package com.innova.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@EnableWebSecurity
+public class _07_AddUserEncrypedBean extends WebSecurityConfigurerAdapter {
+
+    @Bean //Bunu yapı herkesin kullanabilmesini saglar önceki yaptıgımız yapılar ise sadece local yapısındaki birimler kullanır
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder authenticationManagerBuilder) throws Exception {
+        authenticationManagerBuilder
+                .inMemoryAuthentication() //bellek üzerinde olsun db olmasın
+                .withUser("root") //username
+                .password(passwordEncoder().encode("root")) //sifrelenmis data
+                .roles("USER");
+    }
+}
