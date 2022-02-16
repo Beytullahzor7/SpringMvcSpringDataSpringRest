87a75b026f5b0f5a6752d44a724d329c5d8499ca
Beytullahzor7 <beytullahzor7@gmail.com>
2022-02-16 17:30:31 +0300

    Public,Private Pages Security

--- src/main/java/com/innova/security/_07_AddUserEncrypedBean.java
+++ src/main/java/com/innova/security/_07_AddUserEncrypedBean.java
@@ -9,20 +9,20 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
-@EnableWebSecurity
+//@EnableWebSecurity
 public class _07_AddUserEncrypedBean extends WebSecurityConfigurerAdapter {
 
-    @Bean //Bunu yapı herkesin kullanabilmesini saglar önceki yaptıgımız yapılar ise sadece local yapısındaki birimler kullanır
-    public PasswordEncoder passwordEncoder(){
-        return new BCryptPasswordEncoder();
-    }
-
-    @Autowired
-    public void configureGlobal(AuthenticationManagerBuilder authenticationManagerBuilder) throws Exception {
-        authenticationManagerBuilder
-                .inMemoryAuthentication() //bellek üzerinde olsun db olmasın
-                .withUser("root") //username
-                .password(passwordEncoder().encode("root")) //sifrelenmis data
-                .roles("USER");
-    }
+//    @Bean //Bunu yapı herkesin kullanabilmesini saglar önceki yaptıgımız yapılar ise sadece local yapısındaki birimler kullanır
+//    public PasswordEncoder passwordEncoder(){
+//        return new BCryptPasswordEncoder();
+//    }
+//
+//    @Autowired
+//    public void configureGlobal(AuthenticationManagerBuilder authenticationManagerBuilder) throws Exception {
+//        authenticationManagerBuilder
+//                .inMemoryAuthentication() //bellek üzerinde olsun db olmasın
+//                .withUser("root") //username
+//                .password(passwordEncoder().encode("root")) //sifrelenmis data
+//                .roles("USER");
+//    }
 }
---
+++ src/main/java/com/innova/security/_08_SecurityAuthentication.java
@@ -0,0 +1,32 @@
+package com.innova.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity
+public class _08_SecurityAuthentication extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .antMatchers("/security/public","/security/success") //Bu @GetMapping izin vediğim url
+                .permitAll() //yukarıdaki sayfalara login olmadan izin ver
+                .anyRequest()
+                .authenticated()
+                .and()
+                .formLogin();
+    }
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder authenticationManagerBuilder) throws Exception {
+        authenticationManagerBuilder
+                .inMemoryAuthentication()
+                .withUser("root2")
+                .password("{noop}root2")
+                .roles("USER");
+    }
+}
